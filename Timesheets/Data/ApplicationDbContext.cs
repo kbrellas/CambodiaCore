5ba326f870438f2e6625651b98608d97c50d1607
kbrel <kostasmprls@gmail.com>
2019-12-08 19:40:46 +0200

    first commit

---
+++ Timesheets/Data/ApplicationDbContext.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Timesheets.Models;
+
+namespace Timesheets.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<MyUser>
+    {
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<Project> Projects { get; set; }
+        public DbSet<TimesheetEntry> TimesheetEntries { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder); // Required for setting Identity
+                                                // Below are required, to setup the ProductCategory weak entity
+                                                // which connects one or more products to one or more categories
+            modelBuilder.Entity<DepartmentProject>()
+                 .HasKey(t => new { t.DepartmentId, t.ProjectId });
+            modelBuilder.Entity<DepartmentProject>()
+                 .HasOne(pt => pt.Department)
+                 .WithMany(p => p.Projects)
+                 .HasForeignKey(pt => pt.DepartmentId);
+ 				   //.OnDelete(DeleteBehavior.Cascade);   // Setup CASCADE ON DELETE
+            modelBuilder.Entity<DepartmentProject>()
+                 .HasOne(pt => pt.Project)
+                 .WithMany(t => t.Departments)
+                 .HasForeignKey(pt => pt.ProjectId);
+
+            // TODO: Refactor this in order when saving user and including a Department.
+            //       AspNetUsers.DepartmentId should get the correct value!
+            modelBuilder.Entity<Department>()
+                .HasOne(d => d.DepartmentHead)
+                .WithOne(u => u.Department)
+                .HasForeignKey<Department>(ad => ad.DepartmentHeadId);
+
+            modelBuilder.Entity<IdentityRole>().HasData(
+                    new IdentityRole() { Name="Admin",NormalizedName="ADMIN" },
+                    new IdentityRole() { Name="Employee", NormalizedName="EMPLOYEE" },
+                    new IdentityRole() { Name="Manager", NormalizedName="MANAGER"}
+                );
+
+           
+        }
+    }
+}
+
---
+++ Timesheets/Program.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace Timesheets
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // This is a change - gsova! - this is a feature branch change
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
